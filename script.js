b550c7d166c98758ae541d828e23daf1925d9f6b
Fusca1959git <lucasboca2804@gmail.com>
2026-09-09 22:11:28 -0300

    Sim

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let pontos = 0;
 let multiplicador = 1;
+let tempo = 35;
+let timer;
 
 const txtPontos = document.getElementById("pontos");
 const txtMultiplicador = document.getElementById("multiplicador");
+const txtTempo = document.getElementById("tempo")
 const butao = document.getElementById("butao");
-const mensagem = document.getElementById("mensagem");
+const mensagem = document.getElementById("aviso");
 
 butao.onclick = function() {
+    if (tempo === 0){
+        return;
+    }
 
-    pontos += multiplicador
+    pontos += multiplicador;
 
     if (pontos >= 100) {
         multiplicador = 2 ** Math.floor(pontos / 100);
@@ -16,4 +22,22 @@ butao.onclick = function() {
 
     txtPontos.innerText = pontos;
     txtMultiplicador.innerText = multiplicador;
+
+    if (!timer) {
+        timer = setInterval(function() {
+            tempo --;
+            txtTempo.innerText = tempo;
+
+            if (tempo === 0) { 
+                clearInterval(timer);
+                butao.disabled = true
+                timer = null
+
+                mensagem.innerText =
+                    "Tempo esgotado! Você fez " + pontos + " pontos.";
+            }
+        }, 1000)
     }
+}
+
+    
